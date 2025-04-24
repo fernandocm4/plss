b43acdaab3a8d14eff012a6c9a5185eac7879e72
Fernando Candido <fernando@debian.fernando>
2025-04-24 17:54:04 -0300

    menu lateral responsivo

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -11,7 +11,7 @@
 
 <body class="bg-slate-100">
     <div class="flex flex-row h-full w-full">
-        <div class="bg-red-500 rounded-r-lg shadow-xl/30 flex flex-col w-1/6">
+        <div class="bg-red-500 rounded-r-lg shadow-xl/30 flex flex-col md:w-1/2 lg:w-1/4 2xl:w-1/6">
             <div class="flex flex-col py-8 items-center">
                 <a href="{{ route('home') }}"><img class="w-25 pointer-events-none" src="/images/logo.png" alt="logo da empresa PLSS Soluções"></a>
             </div>
